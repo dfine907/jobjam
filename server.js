94687356932b25e0ed54e32c1fd96ffdb0313e94
Dina <dinagiacobbi@gmail.com>
2025-01-31 10:42:24 -0900

    Adding json middleware

--- server.js
+++ server.js
@@ -1,3 +1,18 @@
+import express from 'express'
+const app = express()
+const port = 5000
 
+app.use(express.json())
 
-console.log("server is working...");
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.post("/", (req, res)=> {
+  console.log(req)
+  res.json( {message: "Data received!!", data: req.body})
+})
+
+app.listen(port, () => {
+  console.log(`Server running on ${port}`)
+})
