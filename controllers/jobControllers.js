daa88b2bb42a4dbf8821ee850debf9a93152e65d
Dina <dinagiacobbi@gmail.com>
2025-02-11 08:34:51 -0900

    Refactor updating method in controller

--- controllers/jobControllers.js
+++ controllers/jobControllers.js
@@ -1,11 +1,5 @@
 import 'express-async-errors'
 import Job from '../models/JobModel.js'
-import { nanoid } from 'nanoid'
-
-let jobs = [
-  { id: nanoid(), company: 'apple', position: 'front-end' },
-  { id: nanoid(), company: 'google', position: 'back-end' },
-]
 
 export const getAllJobs = async (req, res) => {
   const jobs = await Job.find({})
@@ -42,28 +36,26 @@ export const getJob = async (req, res) => {
 }
 
 export const updateJob = async (req, res) => {
-  const { company, position } = req.body
-  if (!company || !position) {
-    return res
-      .status(400)
-      .json({ msg: 'Please provide company and position' })
-  }
+  
   const { id } = req.params
-  const job = jobs.find((job) => job.id === id)
-  if (!job) {
+
+  const updatedJob = await Job.findByIdAndUpdate(id, req.body, {new: true})
+  if (!updatedJob) {
     return res.status(404).json({ msg: `No job with ID ${id}` })
   }
-  job.company = company
-  job.position = position
-  res.status(200).json({ msg: 'job updated successfully!', job })
+  
+  res.status(200).json({ msg: 'job updated successfully!', updatedJob })
 }
 
 export const deleteJob = async (req, res) => {
   const { id } = req.params
   const removedJob = await Job.findByIdAndDelete(id)
+
   if (!removedJob) {
     return res.status(404).json({ msg: `No job with ID ${id}` })
   }
 
-  res.status(200).json({ msg: 'job deleted successfully!', job: removedJob})
+  res
+    .status(200)
+    .json({ msg: 'job deleted successfully!', job: removedJob })
 }
