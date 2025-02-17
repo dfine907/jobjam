0a5e6cacd163813a4543bb4cbe62be323f64ed0f
Dina <dinagiacobbi@gmail.com>
2025-02-17 13:31:52 -0900

    Setting up express validator middleware

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import express from 'express'
 const app = express()
 import morgan from 'morgan'
 import mongoose from 'mongoose'
+import { body, validationResult } from 'express-validator'
 
 // my routers:
 import jobRouter from './routes/jobRouter.js'
@@ -21,18 +22,31 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.post('/', (req, res) => {
-  console.log(req)
-  res.json({ message: 'Data received!!', data: req.body })
+// * * * *  TESTING VALIDATIONS:
+app.post('/api/v1/test', [
+  body('name').notEmpty().withMessage('name is required')
+], (req, res, next)=> {
+  const errors = validationResult(req)
+  if(!errors.isEmpty()){
+    const errorMessages = errors.array().map((error)=> error.msg)
+    return res.status(400).json({errors: errorMessages})
+  }
+  
+
+  next()
+},
+(req, res) => {
+  const { name } = req.body
+  res.json({ msg: `hello ${name}` })
 })
 
 app.use('/api/v1/jobs', jobRouter)
 
 //below middleware will take care of the REQUESTS, but not the server
-app.use('*', (req, res)=> {
-  res.status(404).json( {msg: "not found"} )
+app.use('*', (req, res) => {
+  res.status(404).json({ msg: 'not found' })
 })
-//gets triggered by existing controllers and the express-async-errors package 
+//gets triggered by existing controllers and the express-async-errors package
 // app.use((err, req, res, next)=> {
 //   console.log(err)
 //   res.status(500).json({msg: "Oops! Something went wrong!"})
@@ -51,7 +65,3 @@ try {
   console.log(error)
   process.exit(1)
 }
-
-
-
-
